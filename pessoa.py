4d203f7292750790b47641b40cda5070cba2b83d
Roberth <126174269+Roberth-Israel@users.noreply.github.com>
2024-03-14 10:33:40 -0300

    Add files via upload

---
+++ pessoa.py
@@ -0,0 +1,12 @@
+import uuid
+class Pessoa:
+    def __init__(self, nome: str, idade: int):
+        self.matricula = uuid.uuid4()
+        self.nome = nome
+        self.idade = idade
+
+    def __repr__(self):
+        return f'matricula: {self.matricula}\nnome: {self.nome}\nidade: {self.idade}'
+    
+if __name__ == '__main__':
+    print(Pessoa('jonas', 18))
